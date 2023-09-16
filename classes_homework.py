df2e3fe955a581076675336f3c493367d48886e5
eugiv <eugene.ivanov@ymail.com>
2023-09-16 22:13:54 +0300

    job is done

--- classes_homework.py
+++ classes_homework.py
@@ -1,4 +1,21 @@
 import random
+
+
+# global function to calculate average values where it needed
+def avg_grade_glob(grades):
+    ttl_sum = 0
+    ppl_count = 0
+    for key, val in grades.items():
+        if isinstance(val, list):
+            ttl_sum += sum(val)
+            ppl_count += len(val)
+        else:
+            print("Wrong data structure")
+    avg_gr = ttl_sum / ppl_count
+    avg_gr = round(avg_gr, 2)
+    return avg_gr
+
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -18,9 +35,13 @@ class Student:
         else:
             return "Error"
 
+    def avg_grades(self):
+        return avg_grade_glob(self.grades)
+
     def __str__(self):
         return (f'Name: {self.name}\n'
                 f'Surname: {self.surname}\n'
+                f'Average homework grades: {self.avg_grades()}\n'
                 f'Courses in progress: {", ".join(self.courses_in_progress)}\n'
                 f'Finished courses: {", ".join(self.finished_courses)}')
 
@@ -41,9 +62,13 @@ class Lecturer(Mentor):
         super().__init__(name, surname, gender, courses_attached=None)
         self.grades = {}
 
+    def avg_grades(self):
+        return avg_grade_glob(self.grades)
+
     def __str__(self):
         return (f'Name: {self.name}\n'
-                f'Surname: {self.surname}')
+                f'Surname: {self.surname}\n'
+                f'Average lecturers grade: {self.avg_grades()}')
 
 
 class Reviewer(Mentor):
@@ -77,6 +102,7 @@ lecturer_dict = {"name": ["Harland", "Ronald", "Marge"], "surname": ["Sanders", 
 initial_course = ["Python", "Java", "Python", "Kotlin"]
 
 
+# custom functions
 def cls_inst_maker(cls, data):
     Person = {}
     for key in range(len(data["name"])):
@@ -95,23 +121,31 @@ def random_grades(begin, end, k):
     return random.choices(range(begin, end), k=k)
 
 
+def assign_courses_progress(persons: dict, init_course: list):
+    for obj in zip(persons.values(), init_course):
+        obj[0].courses_in_progress += [obj[1]]
+
+
+def assign_courses_attached(persons: dict, init_course: list):
+    for obj in zip(persons.values(), init_course):
+        obj[0].courses_attached += [obj[1]]
+
+
+# creating class instances
 students = cls_inst_maker(Student, students_dict)
 reviewers = cls_inst_maker(Reviewer, reviewers_dict)
 lecturers = cls_inst_maker(Lecturer, lecturer_dict)
 
 # assigning basic course to students
-for stud_obj in zip(students.values(), initial_course):
-    stud_obj[0].courses_in_progress += [stud_obj[1]]
+assign_courses_progress(students, initial_course)
 
 # assigning basic course to reviewers
-for rew_obj in zip(reviewers.values(), initial_course):
-    rew_obj[0].courses_attached += [rew_obj[1]]
+assign_courses_attached(reviewers, initial_course)
 
 # assigning basic course to lecturers
-for lec_obj in zip(lecturers.values(), initial_course):
-    lec_obj[0].courses_attached += [lec_obj[1]]
+assign_courses_attached(lecturers, initial_course)
 
-# custom manual courses adding
+# manual courses adding
 students["student_1"].finished_courses += ["Introduction to Docker"]
 students["student_3"].finished_courses += ["Advanced Git"]
 students["student_3"].courses_in_progress += ["Java"]
@@ -126,16 +160,61 @@ reviewers["reviewer_1"].rate_hw_rew(students["student_3"], "Java", random_grades
 reviewers["reviewer_1"].rate_hw_rew(students["student_3"], "Kotlin", random_grades(3, 10, 15))
 reviewers["reviewer_2"].rate_hw_rew(students["student_0"], "Python", random_grades(3, 10, 25))
 
-
 # students grade lecturers
 students["student_1"].rate_lc(lecturers["lecturer_1"], "Java", random_grades(3, 10, 10))
 students["student_2"].rate_lc(lecturers["lecturer_0"], "Python", random_grades(3, 10, 15))
 students["student_3"].rate_lc(lecturers["lecturer_2"], "Kotlin", random_grades(3, 10, 10))
+students["student_0"].rate_lc(lecturers["lecturer_2"], "Python", random_grades(3, 10, 10))
 
-def homework_avg_grade():
-    courses_list = []
-    for stud in students.values():
-        courses_list += stud.grades.values()
-    return courses_list
 
-print(homework_avg_grade())
+# grades comparison
+def grades_compare(person1, person2):
+    if person1.avg_grades() > person2.avg_grades():
+        print(f"{person1.surname} ({person1.avg_grades()}) has better average grades than {person2.surname} "
+              f"({person2.avg_grades()})", "\n")
+    else:
+        print(f"{person2.surname} ({person2.avg_grades()}) has better average grades than {person1.surname} "
+              f"({person1.avg_grades()})", "\n")
+
+
+# comparing students average grades
+grades_compare(students["student_0"], students["student_1"])
+
+# comparing students average grades
+grades_compare(lecturers["lecturer_2"], lecturers["lecturer_1"])
+
+
+# field test
+courses_list = ["Java", "Kotlin"]
+
+
+def job_avg_grade(course_name: list, persons: dict) -> float:
+    persons_on_course = {}
+    for key, pers in persons.items():
+        for crs in course_name:
+            if crs in pers.grades.keys():
+                persons_on_course[key] = pers.grades[crs]
+    print(persons_on_course)
+
+    return avg_grade_glob(persons_on_course)
+
+
+print("STUDENT:")
+print("-----------")
+print(students["student_3"], "\n")
+print("LECTURER:")
+print("-----------")
+print(lecturers["lecturer_1"], "\n")
+print("REVIEWER:")
+print("-----------")
+print(reviewers["reviewer_0"], "\n")
+
+if len(courses_list) > 1:
+    course_qty = "courses"
+else:
+    course_qty = "course"
+
+print(f"Average students grade by {', '.join(courses_list)} {course_qty}: ",
+      job_avg_grade(courses_list, students), "\n")
+print(f"Average lecturers grade by {', '.join(courses_list)} {course_qty}: ",
+      job_avg_grade(courses_list, lecturers), "\n")
